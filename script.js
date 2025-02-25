da5f5a1399cc2701decf29795c0ed95008b8e55e
oihv <kokolas16@gmail.com>
2025-02-25 11:08:48 +0800

    Implement the resize mechanism

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const container = document.querySelector(".container");
 
 const CONTAINER_SIZE = 512;
 
-function drawGrid(gridSize) {
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
+// draw the grid based on the size
+function drawGrid(size) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
       let pixel = document.createElement("div");
       pixel.classList.add("pixel");
 
-      let size = CONTAINER_SIZE / gridSize;
-      pixel.style.width = size;
-      pixel.style.height = size;
+      let length = CONTAINER_SIZE / size;
+      pixel.style.width = `${length}px`;
+      pixel.style.height = `${length}px`;
 
       container.appendChild(pixel);
 
@@ -21,10 +22,20 @@ function drawGrid(gridSize) {
   }
 }
 
+// get the length from the form, and draw the Grid based on that
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // to prevent the browser from refreshing after submit
+
+  var formData = new FormData(e.target);
+
+  drawGrid(formData.get("size"));
+})
+
 const button = document.querySelector("button#clear");
-const pixels = document.querySelectorAll(".pixel");
 
 button.addEventListener("click", () => {
+  const pixels = document.querySelectorAll(".pixel");
   for (const p of pixels) {
     p.style.backgroundColor = "Blue";
   }
